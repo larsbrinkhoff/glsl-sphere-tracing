2f52123720ca1f06cfee92e336b3f17a0f788881
Lars Brinkhoff <lars@nocrew.org>
2021-11-05 07:48:38 +0100

    Fix window name.

--- sphere-tracing.c
+++ sphere-tracing.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
 	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutCreateWindow("Mindlapse :: GLSL Mandelbrot");
+	glutCreateWindow("Sphere Tracing");
 
 	glutDisplayFunc(draw);
 	glutIdleFunc(idle_handler);
